9dd590312c7e874a3ccc84349e4c4e896eff2a9a
xubin <nybux.tsui@gmail.com>
2022-07-09 14:34:32 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
-void main() {
+void main(List<String> args) {
   runApp(const MyApp());
 }
 
@@ -52,14 +52,25 @@ class _MyHomePageState extends State<MyHomePage> {
   ImageProvider<Object>? preview;
 
   Future<List<Page>> loadPdf(XFile file) async {
-    var b = await file.readAsBytes();
-    int i = 1;
     List<Page> list = [];
-    await for (var page in Printing.raster(b)) {
-      var png = await page.toPng();
-      list.add(Page(png, true, i));
+    var filename = file.name.toLowerCase();
+    if (filename.endsWith(".jpg") ||
+        filename.endsWith(".bmp") ||
+        filename.endsWith(".tiff") ||
+        filename.endsWith(".png")) {
+      list.add(Page(await file.readAsBytes(), true, 1));
+      return list;
+    } else {
+      var b = await file.readAsBytes();
+      int i = 1;
+      await for (var page in Printing.raster(b)) {
+        var x = i;
+        var png = await page.toPng();
+        list.add(Page(png, true, x));
+        i++;
+      }
+      return list;
     }
-    return list;
   }
 
   void save() async {
@@ -89,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
       preview = image;
     });
   }
-  
+
   void _handleClosePreview() {
     setState(() {
       preview = null;
@@ -106,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     DropTarget droper = DropTarget(
       onDragDone: (detail) async {
-        EasyLoading.show(status: '加载...');
+        EasyLoading.show(status: 'LOADING...');
         for (var file in detail.files) {
           List<Page> pages = await loadPdf(file);
           _list.addAll(pages);
@@ -124,7 +135,8 @@ class _MyHomePageState extends State<MyHomePage> {
           _dragging = false;
         });
       },
-      child: const Image( height: 28, width: 50, image: AssetImage('graphics/upload.jpg')),
+      child: const Image(
+          height: 28, width: 50, image: AssetImage('graphics/upload.jpg')),
     );
 
     int i = 0;
@@ -134,13 +146,22 @@ class _MyHomePageState extends State<MyHomePage> {
       var image = MemoryImage(item.image);
       list.add(Column(children: [
         InkWell(
-            onTap: () {_handlePreview(image);},
+            onTap: () {
+              _handlePreview(image);
+            },
             child: Ink.image(image: image, height: 200, width: 150)),
         SizedBox(
             width: 150,
             child: Row(
               children: [
-                Checkbox( value: _list[x].checked, onChanged: (value) { setState(() { _list[x].checked = value!; }); },),
+                Checkbox(
+                  value: _list[x].checked,
+                  onChanged: (value) {
+                    setState(() {
+                      _list[x].checked = value!;
+                    });
+                  },
+                ),
                 Text(_list[x].pageNum.toString())
               ],
             )),
@@ -153,14 +174,24 @@ class _MyHomePageState extends State<MyHomePage> {
         children: [
           Row(children: [
             droper,
-            ElevatedButton( onPressed: _handleCleanList, child: const Text("删除所有")),
-            ElevatedButton( onPressed: save, child: const Text("导出")),
+            ElevatedButton(
+                onPressed: _handleCleanList, child: const Text("DELETE")),
+            ElevatedButton(onPressed: save, child: const Text("EXPORT")),
           ]),
-          Expanded(child: SizedBox(width: double.infinity, child: SingleChildScrollView(child: Wrap( spacing: 8.0, runSpacing: 8.0, children: list,)))),
+          Expanded(
+              child: SizedBox(
+                  width: double.infinity,
+                  child: SingleChildScrollView(
+                      child: Wrap(
+                    spacing: 8.0,
+                    runSpacing: 8.0,
+                    children: list,
+                  )))),
         ],
       );
     } else {
-      child = InkWell( onTap: _handleClosePreview, child: Ink.image(image: preview!));
+      child = InkWell(
+          onTap: _handleClosePreview, child: Ink.image(image: preview!));
     }
 
     return child;
